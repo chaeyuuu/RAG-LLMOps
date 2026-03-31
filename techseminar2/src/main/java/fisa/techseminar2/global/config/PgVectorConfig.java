3817ce383ea23daf2588af4b77e70655845abbe9
Chae-Yu <chaeyu2004@naver.com>
2026-03-31 23:21:05 +0900

    refactor: pgvector Config에 JdbcTemplate 직접 주입하는 방법으로 변경

--- techseminar2/src/main/java/fisa/techseminar2/global/config/PgVectorConfig.java
+++ techseminar2/src/main/java/fisa/techseminar2/global/config/PgVectorConfig.java
@@ -1,31 +1,42 @@
 package fisa.techseminar2.global.config;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
+import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 public class PgVectorConfig {
 
     @Value("${pgvector.datasource.url}")
-    private String pgvectorUrl;
+    private String url;
 
     @Value("${pgvector.datasource.username}")
-    private String pgvectorUsername;
+    private String username;
 
     @Value("${pgvector.datasource.password}")
-    private String pgvectorPassword;
+    private String password;
 
-    @Bean(name = "pgVectorDataSource")
-    public DataSource pgVectorDataSource() {
-        return DataSourceBuilder.create()
-                .url(pgvectorUrl)
-                .username(pgvectorUsername)
-                .password(pgvectorPassword)
-                .driverClassName("org.postgresql.Driver")
+    @Bean
+    public JdbcTemplate pgVectorJdbcTemplate() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public PgVectorStore vectorStore(
+            JdbcTemplate pgVectorJdbcTemplate,
+            EmbeddingModel embeddingModel) {
+        return PgVectorStore.builder(pgVectorJdbcTemplate, embeddingModel)
+                .dimensions(768)
+                .initializeSchema(true)
                 .build();
     }
 }
