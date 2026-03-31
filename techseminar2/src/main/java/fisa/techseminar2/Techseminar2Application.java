0783227e2e1b75e5bd04bc16a6fe9ba621701891
Chae-Yu <chaeyu2004@naver.com>
2026-03-31 09:33:58 +0900

    setting: initial setting

---
+++ techseminar2/src/main/java/fisa/techseminar2/Techseminar2Application.java
@@ -0,0 +1,13 @@
+package fisa.techseminar2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Techseminar2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Techseminar2Application.class, args);
+	}
+
+}
---
+++ techseminar2/src/test/java/fisa/techseminar2/Techseminar2ApplicationTests.java
@@ -0,0 +1,13 @@
+package fisa.techseminar2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Techseminar2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
