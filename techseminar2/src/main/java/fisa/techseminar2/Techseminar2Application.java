6058104b3bebebd5d0ac110dc1133f5ea3420c0f
Chae-Yu <chaeyu2004@naver.com>
2026-04-03 00:45:19 +0900

    feat: pgvectorConfig 스캔 제외

--- techseminar2/src/main/java/fisa/techseminar2/Techseminar2Application.java
+++ techseminar2/src/main/java/fisa/techseminar2/Techseminar2Application.java
@@ -1,9 +1,10 @@
 package fisa.techseminar2;
 
+import org.springframework.ai.autoconfigure.vectorstore.pgvector.PgVectorStoreAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {PgVectorStoreAutoConfiguration.class})
 public class Techseminar2Application {
 
 	public static void main(String[] args) {
