e828823bcac3c9761581ffba1dc2e46e96fe6db9
Chae-Yu <chaeyu2004@naver.com>
2026-04-04 22:14:06 +0900

    feat: @EnableScheduling 어노테이션 추가

--- techseminar2/src/main/java/fisa/techseminar2/Techseminar2Application.java
+++ techseminar2/src/main/java/fisa/techseminar2/Techseminar2Application.java
@@ -3,8 +3,10 @@ package fisa.techseminar2;
 import org.springframework.ai.autoconfigure.vectorstore.pgvector.PgVectorStoreAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = {PgVectorStoreAutoConfiguration.class})
+@EnableScheduling
 public class Techseminar2Application {
 
 	public static void main(String[] args) {
